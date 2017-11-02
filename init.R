59e0ebcde8f28da7786d45dff04e6c90f50accb7
Thomas Lin Pedersen <thomasp85@gmail.com>
2017-11-02 22:26:45 +0100

    ref my repo correctly

--- init.R
+++ init.R
@@ -1,4 +1,4 @@
 install.packages('devtools')
-devtools::install_github('reqres')
-devtools::install_github('fiery')
+devtools::install_github('thomasp85/reqres')
+devtools::install_github('thomasp85/fiery')
 install.packages('routr')
